a8921551c98f4becb93cfc7412ee899718a9468f
soficia <let942@gmail.com>
2022-10-17 16:59:31 -0300

    feat: retorna  o nome e vendidos dos lanches que tenham sido vendidos com uma quantidade diferente de 50 e em que o campo tags não exista

---
+++ challenges/desafio7.js
@@ -0,0 +1,10 @@
+db.produtos.find(
+  {
+    $and:
+      [
+        { vendidos: { $ne: 50 } },
+        { tags: { $exists: false } },
+      ],
+  },
+  { _id: false, nome: true, vendidos: true },
+);

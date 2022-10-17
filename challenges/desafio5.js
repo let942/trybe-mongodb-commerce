28dad84d2826e7008db5ce48ac2dad6224c3263f
soficia <let942@gmail.com>
2022-10-17 16:45:59 -0300

    fix: retorna o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,8 @@
-db.produtos.find({
-  $or: [{ curtidas: { $eq: 36 } }, { vendas: { $eq: 85 } }]
-},
-  { _id: false, nome: true, curtidas: true, vendidos: true });
+db.produtos.find(
+  {
+    $or:
+      [{ curtidas: { $eq: 36 } },
+      { vendidos: { $eq: 85 } }],
+  },
+  { _id: false, nome: true, vendidos: true, curtidas: true },
+);

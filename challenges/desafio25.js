2222fe1258530c37363df352be6d0653d16b566a
soficia <let942@gmail.com>
2022-10-20 09:40:42 -0300

    feat: adiciona o valor muito sódio ao final do array tags nos produtos em que o percentual de sódio seja maior ou igual a 40

---
+++ challenges/desafio25.js
@@ -0,0 +1,20 @@
+db.produtos.updateOne(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "sódio",
+        percentual: {
+          $gt: 40,
+        },
+      },
+    }
+  },
+  {
+    $push: {
+      tags: {
+        $each: ["muito sódio"],
+      },
+    },
+  },
+);
+db.produtos.find({}, { nome: true, tags: true, _id: false });
